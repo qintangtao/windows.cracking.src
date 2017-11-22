ddefb0e81be9aae70ffe4ef55eada4548316609f
qtt <qintangtao@apkshow.com>
2017-11-22 11:54:32 +0800

    rm kstools

--- kstools/apksign.bat
+++
@@ -1,3 +0,0 @@
-cd %~dp0
-java -Xmx2048m -XX:-UseParallelGC -XX:MinHeapFreeRatio=15 -jar kstools.jar ++sign %~n1
-pause
--- kstools/kstools.bat
+++
@@ -1,5 +0,0 @@
-cd %~dp0
-set aapt_path=.\aapt.exe
-java -Xmx2048m -XX:-UseParallelGC -XX:MinHeapFreeRatio=15 -jar kstools.jar ++hook %~dp0 src.apk %aapt_path% 1338303158
-adb install -r signed.apk
-pause..
